b7d66220603b0dc2fe969eecafb749ff8c2f11a6
SHShadhin <princeshadhin70@gmail.com>
2026-01-22 20:37:16 +0600

    practice number

---
+++ number.js
@@ -0,0 +1,21 @@
+var orangePrice = 20;
+
+var chocolate = 15.50;
+chocolate = parseInt(chocolate)
+
+console.log(orangePrice + chocolate)
+
+var num1 = 45.15385;
+var num2 = 12.2537;
+var total = (num1 + num2)
+total = total.toFixed(2)
+total = parseFloat(total)
+console.log(total)
+
+
+// kono kisu ke 0 diye devide korle infinity ase 
+const num = 50 / 0
+console.log(num)
+
+const num3 = -50 / 0
+console.log(num3)
---
+++ types.js
@@ -0,0 +1,12 @@
+var age = 27;
+console.log(age)
+console.log(typeof age)
+
+
+var myName = "Shadhin";
+console.log(myName, typeof myName)
+// console.log(typeof myName)
+
+var isRich = true;
+console.log(isRich)
+console.log(typeof isRich)
